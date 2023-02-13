f7a25eadfca4087723db093c241cc2132691fd94
youssouph <thiamasjo@gmail.com>
2023-02-13 14:21:30 +0000

    commit

--- src/main/java/com/udemy/courudemy/Service/impl/ClientsServiceImplement.java
+++ src/main/java/com/udemy/courudemy/Service/impl/ClientsServiceImplement.java
@@ -2,9 +2,12 @@ package com.udemy.courudemy.Service.impl;
 
 import com.udemy.courudemy.DTO.ClientDTO;
 import com.udemy.courudemy.Service.ClientService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
+@Slf4j
 public class ClientsServiceImplement implements ClientService {
     @Override
     public ClientDTO save(ClientDTO dto) {
